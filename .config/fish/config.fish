7bb95570f0d0d68340e47cf0105c13212dcda479
Ahmad Husen <hapakaien@gmail.com>
2021-02-08 14:05:15 +0700

    feat: Add fish shell config

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+set -x GOROOT /usr/lib/go
+set -x GOPATH $HOME/projects/go
+
+set -x PATH $PATH $GOROOT/bin $GOPATH/bin $HOME/.node_modules/bin
+
+set -x npm_config_prefix $HOME/.node_modules
+
+
+starship init fish | source
