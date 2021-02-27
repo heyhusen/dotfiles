81238dce13e5569621eb3a28d99dfe5f01748b74
Ahmad Husen <hi@husen.id>
2021-02-27 08:07:50 +0700

    feat: add oh-my-fish & change fish theme

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,4 @@
+# Set PATH
 set -x GOROOT /usr/lib/go
 set -x GOPATH $HOME/go
 
@@ -5,5 +6,13 @@ set -x PATH $PATH $GOROOT/bin $GOPATH/bin $HOME/.node_modules/bin
 
 set -x npm_config_prefix $HOME/.node_modules
 
+# Set fish shell
+set -g fish_prompt_pwd_dir_length 0
 
-starship init fish | source
+
+# Set bobthefish theme
+set -g theme_color_scheme nord
+set -g theme_date_timezone Asia/Jakarta
+set -g theme_powerline_fonts yes
+set -g theme_display_git yes
+set -g theme_newline_cursor clean
