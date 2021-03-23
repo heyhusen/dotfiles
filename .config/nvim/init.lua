a976d0f1b0646fa8c9fd9deabfe0ce15dd2a13bb
Ahmad Husen <hi@husen.id>
2021-03-23 22:31:02 +0700

    fix: migrate neovim to lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,63 @@
+-- paq.nvim automatic install
+local install_path = vim.fn.stdpath("data") .. "/site/pack/paq/opt/paq-nvim"
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.cmd("!git clone https://github.com/savq/paq-nvim " .. install_path)
+end
+
+
+-- Install plugins
+require('plugins')
+
+
+-- Settings
+local cmd = vim.cmd
+local indent = 4
+local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
+
+local function opt(scope, key, value)
+  scopes[scope][key] = value
+  if scope ~= 'o' then scopes['o'][key] = value end
+end
+
+cmd 'syntax enable'
+cmd 'filetype plugin indent on'
+opt('b', 'fileencoding', 'utf-8')
+opt('o', 'fileencodings', 'utf-8')
+opt('w', 'number', true)
+opt('w', 'number', true)
+opt('w', 'linebreak', true)
+opt('o', 'showbreak', '+++')
+opt('b', 'textwidth', 80)
+opt('o', 'showmatch', true)
+opt('o', 'visualbell', true)
+opt('w', 'cursorline', true)
+opt('o', 'hidden', true)
+opt('o', 'hlsearch', true)
+opt('o', 'smartcase', true)
+opt('o', 'ignorecase', true)
+opt('o', 'joinspaces', false)
+opt('o', 'incsearch', true)
+opt('b', 'autoindent', true)
+opt('b', 'shiftwidth', indent)
+opt('b', 'smartindent', true)
+opt('o', 'smarttab', true)
+opt('b', 'softtabstop', indent)
+opt('o', 'ruler', true)
+opt('o', 'backup', false)
+opt('o', 'writebackup', false)
+opt('o', 'swapfile', false)
+opt('o', 'cmdheight', 2)
+opt('o', 'updatetime', 300)
+opt('o', 'shortmess', 'c')
+opt('o', 'undolevels', 1000)
+opt('o', 'backspace', 'indent,eol,start')
+opt('o', 'showmode', false)
+opt('o', 'switchbuf', 'usetab,newtab')
+opt('w', 'signcolumn', 'number')
+opt('o', 'termguicolors', true)
+cmd 'colorscheme zephyr'
+
+
+-- Configs
+require('config')
+
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,12 @@
+vim.cmd "packadd paq-nvim"
+local paq = require "paq-nvim".paq
+
+paq{'savq/paq-nvim', opt=true}
+paq{'nvim-treesitter/nvim-treesitter', run=vim.fn[':TSUpdate']}
+paq 'glepnir/zephyr-nvim'
+paq 'norcalli/nvim-colorizer.lua'
+
+local plugin_path = vim.fn.stdpath("data") .. "/site/pack/paqs"
+if vim.fn.empty(vim.fn.glob(plugin_path)) > 0 then
+    vim.cmd[[ PaqInstall ]]
+end
