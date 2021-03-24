481078ebd1c8f15238755822b4e01d2a3d639d4b
Ahmad Husen <hi@husen.id>
2021-03-24 14:59:31 +0700

    feat: remove post-install hook from nvim-treesitter

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -2,7 +2,7 @@ vim.cmd "packadd paq-nvim"
 local paq = require "paq-nvim".paq
 
 paq{'savq/paq-nvim', opt=true}
-paq{'nvim-treesitter/nvim-treesitter', run=vim.fn[':TSUpdate']}
+paq{'nvim-treesitter/nvim-treesitter'}
 paq 'glepnir/zephyr-nvim'
 paq 'kyazdani42/nvim-web-devicons'
 paq 'yamatsum/nvim-web-nonicons'
