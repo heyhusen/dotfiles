b622dcc07ea1ee538fe4f90046795b721135cf78
Ahmad Husen <hi@husen.id>
2021-05-08 21:06:38 +0700

    feat: exclude markdown from indent guide on nvim

--- .config/nvim/lua/config/indent-guides.lua
+++ .config/nvim/lua/config/indent-guides.lua
@@ -1,5 +1,5 @@
 require('indent_guides').setup({
-    exclude_filetypes = {'help', 'dashboard', 'dashpreview', 'NvimTree', 'vista', 'sagahover', 'minimap'};
+    exclude_filetypes = {'help', 'dashboard', 'dashpreview', 'NvimTree', 'vista', 'sagahover', 'minimap', 'markdown'};
     -- indent_tab_guides = true;
 })
 
