66dd35b0e501beb68d497d981305793bed7c7e2c
Ahmad Husen <hi@husen.id>
2021-09-29 12:57:00 +0700

    Update private_dot_config/private_fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -31,3 +31,5 @@ set -gx GPG_TTY (tty)
 # tabtab source for packages
 # uninstall by removing these lines
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
+
+starship init fish | source
