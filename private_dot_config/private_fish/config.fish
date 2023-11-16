4de1997c5bcbff4891e2a499dc773e71ecd5016c
Husen <hi@husen.id>
2023-11-16 17:12:53 +0700

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -15,12 +15,3 @@ set -gx GPG_TTY (tty)
 [ -f ~/.config/tabtab/fish/__tabtab.fish ]; and . ~/.config/tabtab/fish/__tabtab.fish; or true
 
 starship init fish | source
-
-# pnpm
-set -gx PNPM_HOME "/home/hapakaien/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
-set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
