0e8bd4b943b972591148fb5ca257505f805eff9e
Husen <hi@husen.id>
2023-11-16 17:03:55 +0700

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -16,3 +16,11 @@ set -gx GPG_TTY (tty)
 
 starship init fish | source
 
+# pnpm
+set -gx PNPM_HOME "/home/hapakaien/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
